7081d3256499fb7e59d8ec5efe319bb82ddba324
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-12-27 18:41:02 -0300

    feat(ast): implement Display trait to make tests easier

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,14 @@
 use crate::token::*;
+use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub struct Expression {}
+
+impl fmt::Display for Expression {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "hello world")
+  }
+}
 
 #[derive(Debug, PartialEq)]
 pub struct LetStatement {
@@ -10,16 +17,54 @@ pub struct LetStatement {
   //value: Expression,
 }
 
+impl fmt::Display for LetStatement {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match &self.identifier {
+      Token::Identifier(identifier) => write!(f, "Token({:?})", identifier),
+      _ => unreachable!(),
+    }
+  }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct ReturnStatement {
   pub token: Token,
   pub value: Expression,
 }
 
+impl fmt::Display for ReturnStatement {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "Return")
+  }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ExpressionStatement {
+  pub token: Token,
+  expression: Expression,
+}
+
+impl fmt::Display for ExpressionStatement {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "Expression({:?})", self.expression)
+  }
+}
+
 #[derive(Debug, PartialEq)]
 pub enum Statement {
   Let(LetStatement),
   Return(ReturnStatement),
+  Expression(ExpressionStatement),
+}
+
+impl fmt::Display for Statement {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      Statement::Let(statement) => statement.fmt(f),
+      Statement::Return(statement) => statement.fmt(f),
+      Statement::Expression(statement) => statement.fmt(f),
+    }
+  }
 }
 
 #[derive(Debug, PartialEq)]
