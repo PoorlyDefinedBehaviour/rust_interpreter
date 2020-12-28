fe02a5678e628e43088aedc14ffcabafa0481658
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-12-28 18:28:51 -0300

    feat(token): implement fmt::Display for Token to make tests easier

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, PartialEq, Clone, Eq, Hash)]
 pub enum Token {
   Illegal(char),
@@ -23,6 +25,36 @@ pub enum Token {
   Function,
   Let,
   Return,
+}
+
+impl fmt::Display for Token {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      Token::Illegal(char) => write!(f, "{}", char),
+      Token::Eof => write!(f, "EOF"),
+      Token::Identifier(identifier) => write!(f, "{}", identifier),
+      Token::Number(number) => write!(f, "{}", number),
+      Token::Assign => write!(f, "="),
+      Token::Plus => write!(f, "+"),
+      Token::Minus => write!(f, "-"),
+      Token::Star => write!(f, "*"),
+      Token::Slash => write!(f, "/"),
+      Token::GreaterThan => write!(f, ">"),
+      Token::LessThan => write!(f, "<"),
+      Token::Equal => write!(f, "=="),
+      Token::NotEqual => write!(f, "!="),
+      Token::Bang => write!(f, "!"),
+      Token::Comma => write!(f, ","),
+      Token::Semicolon => write!(f, ";"),
+      Token::LeftParen => write!(f, "("),
+      Token::RightParen => write!(f, ")"),
+      Token::LeftBrace => write!(f, "{{"),
+      Token::RightBrace => write!(f, "}}"),
+      Token::Function => write!(f, "fn()"),
+      Token::Let => write!(f, "let"),
+      Token::Return => write!(f, "return"),
+    }
+  }
 }
 
 pub fn lookup_identifier(lexeme: String) -> Token {
