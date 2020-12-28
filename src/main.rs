6277e075807129fe61588998862b053d0dcc4069
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-12-27 21:37:02 -0300

    feat(parser): parse numbers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,1 @@
-use interpreter::lexer::Lexer;
-use interpreter::parser::Parser;
-
-fn main() {
-  let mut lexer = Lexer::new(String::from("let a = 10"));
-  let tokens = lexer.lex();
-
-  println!("tokens: {:?}", tokens);
-
-  println!("statements: {:?}", Parser::new(tokens).parse());
-}
+fn main() {}
