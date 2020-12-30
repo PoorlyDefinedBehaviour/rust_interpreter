6b7f6e7d6464df8e05b817ddb801e719d16f3a6f
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-12-30 19:51:23 -0300

    feat: add simple repl

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,29 @@
-fn main() {}
+use interpreter::lexer::Lexer;
+use interpreter::parser::Parser;
+use std::io::{self, Write};
+
+fn main() {
+  loop {
+    print!("> ");
+
+    io::stdout().flush().expect("flush failed");
+
+    let mut buffer = String::new();
+
+    io::stdin()
+      .read_line(&mut buffer)
+      .expect("unable to read input");
+
+    let mut lexer = Lexer::new(buffer);
+
+    let mut parser = Parser::new(lexer.lex());
+
+    let program = parser.parse();
+
+    if program.has_errors() {
+      dbg!(program.errors);
+    } else {
+      dbg!(program.statements);
+    }
+  }
+}
